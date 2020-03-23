7db447d1a2c737b01102b0f982417b547b28c773
putriendah11 <putriendah11@gmail.com>
2020-03-23 23:23:29 +0700

    code tugas 4

--- tugas4/list.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def files(path):
-    for file in os.listdir(path):
-        if os.path.isfile(os.path.join(path, file)):
-            yield file
-
-for file in files("."):
-    print (file)
